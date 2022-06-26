71bc369837f5ae92414ee6c2864ade080f099bc5
tchanhtinh <tchanhtinh@gmail.com>
2022-06-26 20:44:30 +0700

    init

---
+++ keymaps/tchanhtinh/rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE =no 
+VIA_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+MOUSEKEY_ENABLE = yes
+
+# if firmware size over limit, try this option
+CFLAGS += -flto
+
+WPM_ENABLE = yes
